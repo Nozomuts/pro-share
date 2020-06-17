328e0d7decd1ee095ef167a17cfc054d825516aa
Nozomu Tsuruta <61049074+NozomuTsuruta@users.noreply.github.com>
2020-06-17 16:52:05 +0900

    first commit

---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/store'
+import 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDY6MZXVHvp9qXq0OimRmx13HLqugPLjnc",
+  authDomain: "share-pro-20be9.firebaseapp.com",
+  databaseURL: "https://share-pro-20be9.firebaseio.com",
+  projectId: "share-pro-20be9",
+  storageBucket: "share-pro-20be9.appspot.com",
+  messagingSenderId: "930667147859",
+  appId: "1:930667147859:web:70c8eae75eb462b8154ca6",
+  measurementId: "G-MJ79J4KTTQ"
+};
+
+firebase.initializeApp(firebaseConfig);
